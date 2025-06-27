9936b082912b4b5e92fae6e386a3c6990e2f45bd
Manish <156883335+Manish50518@users.noreply.github.com>
2025-06-27 17:42:36 +0530

    fix:vercel url

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
 
-const authRoutes = require('./Routes/Auth');
+const authRoutes = require("./Routes/Auth");
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://qtm-assign-o4qz.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error(err));
-
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
